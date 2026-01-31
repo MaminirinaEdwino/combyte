9e0325c634584f04df27bac5badebe58362bc157
Maminirina Edwino <edwinomaminirina@gmail.com>
2026-01-31 21:00:38 +0300

    implement: basic strucutre of the cli

--- main.go
+++ main.go
@@ -1,8 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
+
+const(
+	compressText = "This is the option for compressing a text file"
+	extractText = "This is the option for extracting a file .combyte"
+)
 
 func main() {
 	fmt.Println("Combyte CLI")
-	compress := 
+	
+	compress := flag.Bool("compress", false, compressText)
+	c := flag.Bool("c", false, compressText)
+
+	extract := flag.Bool("extract", false, extractText)
+	e := flag.Bool("e", false, extractText)
+
+	filename := flag.String("filename", "", "THe file that you want to extract or compress")
+	flag.Parse()
+	switch  {
+	case *compress || *c:
+		if *filename == "" {
+			break
+		}
+	case *extract || *e:
+		if *filename == "" {
+			break
+		}
+	default : 
+		fmt.Println("Help documentation")
+	}
 }
